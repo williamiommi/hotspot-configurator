9280c1ae08dc72d94a31f0e579517dcb49d7315e
William Iommi <william.iommi@gmail.com>
2022-10-28 11:52:28 +0200

    minor

--- apps/contentful-app/src/locations/Field.tsx
+++ apps/contentful-app/src/locations/Field.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Paragraph } from '@contentful/f36-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
-import { Button } from 'ui';
 
 const Field = () => {
   const sdk = useSDK<FieldExtensionSDK>();
@@ -14,15 +13,7 @@ const Field = () => {
   // If you only want to extend Contentful's default editing experience
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return (
-    <Paragraph>
-      Hello Entry Field Component (AppId: {sdk.ids.app})<span className="bg-emerald-500">CIAO</span>
-      <br />
-      e il monorepo worka ma forse non con pnpm!!!
-      <br />
-      <Button />
-    </Paragraph>
-  );
+  return <Paragraph>Hello Entry Field Component (AppId: {sdk.ids.app})</Paragraph>;
 };
 
 export default Field;
