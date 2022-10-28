342a51ba0ec889778bfa670dbfb0cfa3bd14724b
William Iommi <william.iommi@gmail.com>
2022-10-28 11:32:47 +0200

    minor on configurations

--- packages/tailwind-config/tailwind.config.react.js
+++ packages/tailwind-config/tailwind.config.react.js
@@ -1,5 +1,5 @@
 const config = require('./tailwind.config');
 module.exports = {
   ...config,
-  content: ['src/**/*.{js,ts,jsx,tsx}'],
+  content: ['../../packages/ui/src/**/*.{js,ts,jsx,tsx}', 'src/**/*.{js,ts,jsx,tsx}'],
 };
