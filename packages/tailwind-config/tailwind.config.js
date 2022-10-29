6b274503d2a92f068f14e58a81e1d665936628c4
William Iommi <william.iommi@gmail.com>
2022-10-30 00:18:44 +0200

    tailwind-config: minor on old folder

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['../../packages/ui/src/**/*.{js,ts,jsx,tsx}'],
+  content: ['../../packages/shared/src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
