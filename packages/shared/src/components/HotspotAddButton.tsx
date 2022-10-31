364e36853145e4f90c32be5a5160752a62db9de6
William Iommi <william.iommi@gmail.com>
2022-10-31 22:40:03 +0100

    shared: minor on copy

--- packages/shared/src/components/HotspotAddButton.tsx
+++ packages/shared/src/components/HotspotAddButton.tsx
@@ -10,7 +10,7 @@ export const HotspotAddButton: React.FC<HotspotAddButtonProps> = ({ onClick }) =
       className="my-1 w-full border-2 border-dashed p-3 font-bold text-slate-500 hover:bg-slate-100/30"
       onClick={onClick}
     >
-      + ADD HOTSPOT!!
+      + ADD HOTSPOT
     </button>
   );
 };
