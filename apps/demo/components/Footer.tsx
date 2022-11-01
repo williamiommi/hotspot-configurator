2ca61940865fdd4e64436925c2be72f953e3639c
William Iommi <william.iommi@gmail.com>
2022-11-01 17:49:46 +0100

    demo: minor on footer copy

--- apps/demo/components/Footer.tsx
+++ apps/demo/components/Footer.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link';
-import ExternalLink from './icons/ExternalLink';
 
 const Footer = () => (
   <footer className="flex flex-col items-center justify-center p-5 gap-2 sm:flex-row">
-    <span>All rights reserved &copy; 2022</span>
-    <span className="hidden lg:initial">|</span>
+    <span>Copyright &copy; 2022.</span>
     <Link href="https://williamiommi.me">
-      <a target="_blank" className="flex items-center gap-1 underline hover:opacity-80">
-        William Iommi <ExternalLink />
+      <a target="_blank" className="flex items-center hover:opacity-80">
+        <span className="underline text-blue-700">William Iommi</span>.
       </a>
     </Link>
+    <span>All rights reserved.</span>
   </footer>
 );
 
