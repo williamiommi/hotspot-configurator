7deda01d8b2ffd69ccb56a8c5dc92b4b1fa7b55d
William Iommi <william.iommi@gmail.com>
2022-11-06 22:29:10 +0100

    demo: minor on footer

--- apps/demo/components/Footer.tsx
+++ apps/demo/components/Footer.tsx
@@ -1,14 +1,19 @@
 import Link from 'next/link';
 
 const Footer = () => (
-  <footer className="flex flex-col items-center justify-center p-5 gap-2 sm:flex-row">
-    <span>Copyright &copy; 2022.</span>
-    <Link href="https://williamiommi.me">
-      <a target="_blank" className="flex items-center hover:opacity-80">
-        <span className="underline text-blue-700">William Iommi</span>.
+  <footer className='flex flex-col items-center justify-center gap-2 p-5 sm:flex-row'>
+    <span>
+      Copyright &copy; 2022<span className='hidden sm:inline-block'>.</span>
+    </span>
+    <Link href='https://williamiommi.me'>
+      <a target='_blank' className='flex items-center hover:opacity-80'>
+        <span className='text-blue-700 underline'>William Iommi</span>
+        <span className='hidden sm:inline-block'>.</span>
       </a>
     </Link>
-    <span>All rights reserved.</span>
+    <span>
+      All rights reserved<span className='hidden sm:inline-block'>.</span>
+    </span>
   </footer>
 );
 
