aa9e160cd2f8abca6d04199f646fff70006dfd3e
William Iommi <william.iommi@gmail.com>
2022-12-12 21:24:58 +0100

    removed cf logo

--- apps/demo/components/Navigation.tsx
+++ apps/demo/components/Navigation.tsx
@@ -8,26 +8,25 @@ import { MenuIcon } from '@contentful/f36-icons';
 const Navigation = () => {
   const { pathname } = useRouter();
   return (
-    <header className="flex items-center justify-between py-4 px-3 mb-10 bg-gray-100/70">
-      <Link href="/">
-        <a className="flex items-center gap-4 w-[300px]">
-          <LogoIcon className="w-7 lg:w-10" />
-          <h1 className="font-bold italic lg:text-2xl">Hotspot Configurator</h1>
+    <header className='mb-10 flex items-center justify-between bg-gray-100/70 py-4 px-3'>
+      <Link href='/'>
+        <a className='flex w-[300px] items-center gap-4'>
+          <h1 className='font-bold italic lg:text-2xl'>Hotspot Configurator</h1>
         </a>
       </Link>
-      <nav className="hidden gap-4 flex-grow justify-center lg:flex">
-        <Link href="/playground">
+      <nav className='hidden flex-grow justify-center gap-4 lg:flex'>
+        <Link href='/playground'>
           <a
-            className={`text-2xl border-b border-b-transparent ${
+            className={`border-b border-b-transparent text-2xl ${
               pathname === '/playground' ? '!border-b-black' : ''
             }`}
           >
             Playground
           </a>
         </Link>
-        <Link href="/demo">
+        <Link href='/demo'>
           <a
-            className={`text-2xl border-b border-b-transparent ${
+            className={`border-b border-b-transparent text-2xl ${
               pathname === '/demo' ? '!border-b-black' : ''
             }`}
           >
@@ -35,11 +34,11 @@ const Navigation = () => {
           </a>
         </Link>
       </nav>
-      <div className="hidden w-[300px] justify-end lg:flex">
-        <Link href="">
+      <div className='hidden w-[300px] justify-end lg:flex'>
+        <Link href=''>
           <a
-            target="_blank"
-            className="flex items-center gap-2 bg-blue-700 text-white p-4 rounded-lg transition-colors duration-300 font-bold hover:bg-blue-800"
+            target='_blank'
+            className='flex items-center gap-2 rounded-lg bg-blue-700 p-4 font-bold text-white transition-colors duration-300 hover:bg-blue-800'
           >
             View the article <ExternalLink />
           </a>
@@ -48,17 +47,17 @@ const Navigation = () => {
       <Menu>
         <Menu.Trigger>
           <IconButton
-            className="lg:!hidden"
-            variant="transparent"
+            className='lg:!hidden'
+            variant='transparent'
             icon={<MenuIcon />}
-            aria-label="toggle menu"
+            aria-label='toggle menu'
           />
         </Menu.Trigger>
-        <Menu.List className="!p-0 lg:!hidden">
-          <Menu.Item className="!p-0">
-            <Link href="/playground">
+        <Menu.List className='!p-0 lg:!hidden'>
+          <Menu.Item className='!p-0'>
+            <Link href='/playground'>
               <a
-                className={`p-3 block w-full text-lg ${
+                className={`block w-full p-3 text-lg ${
                   pathname === '/playground' ? 'bg-blue-600/10' : ''
                 }`}
               >
@@ -66,10 +65,10 @@ const Navigation = () => {
               </a>
             </Link>
           </Menu.Item>
-          <Menu.Item className="!p-0">
-            <Link href="/demo">
+          <Menu.Item className='!p-0'>
+            <Link href='/demo'>
               <a
-                className={`p-3 block w-full text-lg ${
+                className={`block w-full p-3 text-lg ${
                   pathname === '/demo' ? 'bg-blue-600/10' : ''
                 }`}
               >
@@ -77,11 +76,11 @@ const Navigation = () => {
               </a>
             </Link>
           </Menu.Item>
-          <Menu.Item className="!py-5 hover:!bg-transparent hover:!cursor-auto">
-            <Link href="">
+          <Menu.Item className='!py-5 hover:!cursor-auto hover:!bg-transparent'>
+            <Link href=''>
               <a
-                target="_blank"
-                className="flex items-center gap-2 bg-blue-700 text-white p-4 rounded-lg transition-colors duration-300 font-bold hover:bg-blue-800"
+                target='_blank'
+                className='flex items-center gap-2 rounded-lg bg-blue-700 p-4 font-bold text-white transition-colors duration-300 hover:bg-blue-800'
               >
                 View the article <ExternalLink />
               </a>
--- apps/demo/pages/index.tsx
+++ apps/demo/pages/index.tsx
@@ -11,26 +11,25 @@ const Home: NextPage = () => {
       <Head>
         <title>Contentful Hotspot Configurator</title>
       </Head>
-      <div className="mt-20 sm:mt-0 sm:grid sm:place-items-center sm:h-screen">
-        <div className="p-2 pb-5 sm:bg-gray-100 sm:rounded-3xl sm:shadow-2xl sm:p-20 sm:pb-5">
-          <div className="pb-20">
-            <Logo className="max-w-xs mx-auto pb-10" />
-            <h1 className="font-bold text-4xl mb-4 text-center">Hotspot Configurator</h1>
-            <div className="flex flex-col text-center items-center gap-4 mt-20">
-              <Link href="/demo">
-                <a className="bg-blue-700 text-white p-4 rounded-lg w-full max-w-96 transition-colors duration-300 font-bold hover:bg-blue-800">
+      <div className='mt-20 sm:mt-0 sm:grid sm:h-screen sm:place-items-center'>
+        <div className='p-2 pb-5 sm:rounded-3xl sm:bg-gray-100 sm:p-20 sm:pb-5 sm:shadow-2xl'>
+          <div className='pb-20'>
+            <h1 className='mb-4 text-center text-4xl font-bold'>Hotspot Configurator</h1>
+            <div className='mt-20 flex flex-col items-center gap-4 text-center'>
+              <Link href='/demo'>
+                <a className='max-w-96 w-full rounded-lg bg-blue-700 p-4 font-bold text-white transition-colors duration-300 hover:bg-blue-800'>
                   DEMO
                 </a>
               </Link>
-              <Link href="/playground">
-                <a className="border border-blue-700 text-black p-4 rounded-lg w-full max-w-96 transition-colors duration-300 font-bold hover:bg-white/70">
+              <Link href='/playground'>
+                <a className='max-w-96 w-full rounded-lg border border-blue-700 p-4 font-bold text-black transition-colors duration-300 hover:bg-white/70'>
                   PLAYGROUND
                 </a>
               </Link>
-              <Link href="/">
+              <Link href='/'>
                 <a
-                  className="underline transition-opacity hover:opacity-70 flex items-center justify-center gap-2"
-                  target="_blank"
+                  className='flex items-center justify-center gap-2 underline transition-opacity hover:opacity-70'
+                  target='_blank'
                 >
                   Visit the article <ExternalLink />
                 </a>
